5f283c2a63aff74c2ed1cca928192eb8bf92bff3
martin-chen <chenhao@linkipr.com>
2018-04-16 17:16:13 +0800

    配置环境

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+// 状态
+const state = {
+
+}
+// type,注册事件
+const actions = {
+
+}
+// 改变状态
+const mutations = {
+
+}
+// 得到数值，做最后的数据整合传导到页面中。
+const getters  = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
+
